e333bb5e7abeeb11568e435a16987d3db1addf5b
andres escobar <andres.escobar.godella@gmail.com>
2023-07-13 09:59:35 +0200

    Mapeo api rest

---
+++ src/main/java/SP04T1N01/S04T01N01EscobarJuliaAndres/DemoApplication.java
@@ -0,0 +1,14 @@
+package SP04T1N01.S04T01N01EscobarJuliaAndres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+      }
+
+}
